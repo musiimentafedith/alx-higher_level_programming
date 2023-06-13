d4f1013051a5e4ec05901693236fda892cfb2e20
odith <musiimentaolix@gmail.com>
2023-06-13 22:03:11 +0300

    fixxed the errors in the for loop range

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     temp = 0
-    for i in range((len(my_list) - 1)):
+    for i in range(len(my_list)):
         if temp == 0 or temp < my_list[i]:
             temp = my_list[i]
     return temp
