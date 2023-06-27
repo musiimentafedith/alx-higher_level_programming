8742e9f5e60ff5d07c65be919586e56a3df9eae0
odith <musiimentafedith.com>
2023-06-27 23:26:20 +0300

    task0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""
+
+This module includes a square class
+
+"""
+
+class Square:
+    """
+    square is an empty class, that is no attributes or methods
+
+    """
+    pass
