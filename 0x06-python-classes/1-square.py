a53874644b133c0d40f3adb20f84b696dc0412a5
odith <musiimentafedith.com>
2023-06-27 23:55:40 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+This moducle has a class named square
+
+
+"""
+
+
+class Square:
+
+    """
+
+    This is the square class
+
+
+    Attributes:
+        __size (int): size is an interger representing how big the square is
+
+    """
+
+    def __init__(self, size):
+        """
+
+        This is called when instanes of the class are intialized
+
+        Args:
+             size (int): this is used to intialize __size
+        """
+        self.__size = size
