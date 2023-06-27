bdaedfa5c342899e10b5d5253bf924db34c68f48
odith <musiimentafedith.com>
2023-06-28 00:20:03 +0300

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+
+This module has a square class
+
+"""
+
+
+class Square:
+    """
+
+    This is a class representiong a square
+
+    Attributes:
+        __size (int): an int representing how big the square is
+
+    """
+
+    def __init__(self, size=0):
+        """
+
+        This is to initalise instances of the class
+
+        Args:
+            size (int): an int representing how big the square is
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
